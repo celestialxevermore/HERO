12ffa024d5d59b4ee356d64910aaa598c70c8466
key <key2317@naver.com>
2024-08-16 22:40:48 +0900

    Custom test, 편의성 개선

--- S2IP-LLM/Long-term_Forecasting/exp/exp_long_term_forecasting.py
+++ S2IP-LLM/Long-term_Forecasting/exp/exp_long_term_forecasting.py
@@ -1,6 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from utils.tools import EarlyStopping, adjust_learning_rate, visual,adjust_model
+from utils.tools import EarlyStopping, adjust_learning_rate, visual,adjust_model, format_time, save_experiment_data
 from utils.metrics import metric
 import torch
 import torch.nn as nn
@@ -10,18 +10,13 @@ from utils.losses import mape_loss, mase_loss, smape_loss
 
 
 from transformers import AdamW
-
-
-
-
-
 from torch.utils.data import Dataset, DataLoader
 from torch import optim
 import os
 import time
 import warnings
 import numpy as np
-
+import matplotlib.pyplot as plt 
 from tqdm import tqdm
 
 warnings.filterwarnings('ignore')
@@ -49,8 +44,6 @@ class Exp_Long_Term_Forecast(object):
 
     def _build_model(self):
         model = self.model_dict[self.args.model].Model(self.args).to(self.device)
-        
-
         return model
 
     def _get_data(self, flag):
@@ -101,10 +94,6 @@ class Exp_Long_Term_Forecast(object):
                 outputs = outputs[:, -self.args.pred_len:, f_dim:self.args.number_variable].float().to(self.device)
                 batch_y = batch_y[:, -self.args.pred_len:, f_dim:self.args.number_variable].float().to(self.device)
 
-            
-
-                
-
                 pred = outputs.detach().cpu()
                 true = batch_y.detach().cpu()
 
@@ -117,24 +106,24 @@ class Exp_Long_Term_Forecast(object):
         self.model.train()
         return total_loss
     
-
-   
-
     def train(self, setting):
 
-        
-        path = os.path.join(self.args.checkpoints, setting)
-        if not os.path.exists(path):
-            os.makedirs(path)
+        base_path = os.path.join("/mnt/storage/personal/eungyeop/HERO/experiments", self.args.exp_info)
+        if not os.path.exists(base_path):
+            os.makedirs(base_path)
 
         time_now = time.time()
 
         train_steps = len(self.train_loader)
         early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
 
+        model_optim = self._select_optimizer()
+        criterion = self._select_criterion()
+
         if self.args.use_amp:
             scaler = torch.cuda.amp.GradScaler()
 
+        train_start = time.time()
         for epoch in range(self.args.train_epochs):
             iter_count = 0
             train_loss = []
@@ -146,7 +135,6 @@ class Exp_Long_Term_Forecast(object):
                 iter_count += 1
                 self.optimizer.zero_grad()
                 batch_x = batch_x.float().to(self.device)
-
                 batch_y = batch_y.float().to(self.device)
                 batch_x_mark = batch_x_mark.float().to(self.device)
                 batch_y_mark = batch_y_mark.float().to(self.device)
@@ -195,7 +183,7 @@ class Exp_Long_Term_Forecast(object):
                     print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
                     speed = (time.time() - time_now) / iter_count
                     left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
-                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    print(f'\tspeed: {speed:.4f}s/iter; left time: {format_time(left_time)}')
                     iter_count = 0
                     time_now = time.time()
 
@@ -206,8 +194,9 @@ class Exp_Long_Term_Forecast(object):
                  
                     loss.backward()
                     self.optimizer.step()
-               
-            print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
+
+            epoch_duration = time.time() - epoch_time
+            print("Epoch : {} cost time : {}".format(epoch + 1, format_time(epoch_duration)))
             train_loss = np.average(train_loss)
             sim_loss = np.average(simlarity_losses)
             vali_loss = self.vali(self.vali_data, self.vali_loader, self.criterion)
@@ -217,29 +206,42 @@ class Exp_Long_Term_Forecast(object):
                 epoch + 1, train_steps, train_loss, vali_loss,sim_loss))
             
             
-            early_stopping(vali_loss, self.model, path)
+            early_stopping(vali_loss, self.model, base_path)
             if early_stopping.early_stop:
                 print("Early stopping")
                 break
 
             adjust_learning_rate(self.optimizer, epoch + 1, self.args)
             adjust_model(self.model, epoch + 1,self.args)
-
-
-            
+        
+        train_duration = time.time() - train_start 
+        print(f"Total training time: {format_time(train_duration)}")
+        model_path = os.path.join(base_path, f"{self.args.exp_protocol}")
+        os.makedirs(model_path, exist_ok = True)
+        torch.save(self.model.state_dict(), os.path.join(model_path,'model_checkpoint.pth'))
+        return    
 
     def test(self, setting, test=1):
 
         
         test_data, test_loader = self._get_data(flag='test')
-
         
-      
+        if test:
+            print('loading model')
+            base_path = os.path.join("/mnt/storage/personal/eungyeop/ETRI_HANDOVER/experiments", self.args.exp_info)
+            if not os.path.exists(base_path):
+                os.makedirs(base_path)
+
+            self.model.load_state_dict(torch.load(os.path.join(base_path, f"{self.args.exp_protocol}", f"exp3 : {self.args.model}", "model_checkpoint.pth")))
+        
+        
+        
+        model_path = os.path.join(base_path, f"{self.args.exp_protocol}", f"exp3 : {self.args.model}")
         preds = []
         trues = []
-        folder_path = './test_results/' + setting + '/'
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+        # folder_path = './test_results/' + setting + '/'
+        # if not os.path.exists(folder_path):
+        #     os.makedirs(folder_path)
         
         sim_matrix = []
         input_embedding = []
@@ -248,11 +250,11 @@ class Exp_Long_Term_Forecast(object):
 
 
         self.model.eval()
+        test_start = time.time()
         with torch.no_grad():
             for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in tqdm(enumerate(test_loader)):
                 batch_x = batch_x.float().to(self.device)
                 batch_y = batch_y.float().to(self.device)
-
                 batch_x_mark = batch_x_mark.float().to(self.device)
                 batch_y_mark = batch_y_mark.float().to(self.device)
 
@@ -297,12 +299,12 @@ class Exp_Long_Term_Forecast(object):
                     input = batch_x.float().detach().cpu().numpy()
                     gt = np.concatenate((input[0, :, -1], true[0, :, -1]), axis=0)
                     pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
-                    visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
+                    visual(gt, pd, os.path.join(model_path, str(i) + f"visual_{self.args.model}.pdf"))
 
-        
-       
+        test_duration = time.time() - test_start
+        print("Test duration: {}".format((format_time(test_duration))))
+
   
-            
         preds = np.array(preds)
         trues = np.array(trues)
             
@@ -310,24 +312,16 @@ class Exp_Long_Term_Forecast(object):
         preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
         trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
         print('test shape:', preds.shape, trues.shape)
-
-        # result save
-        folder_path = './results/' + setting + '/'
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
         mae, mse, rmse, mape, mspe = metric(preds, trues)
-        print('mse:{}, mae:{}'.format(mse, mae))
-        f = open("result_long_term_forecast.txt", 'a')
-        f.write(setting + "  \n")
-        f.write('mse:{}, mae:{}'.format(mse, mae))
-        f.write('\n')
-        f.write('\n')
-        f.close()
 
-        np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
-        np.save(folder_path + 'pred.npy', preds)
-        np.save(folder_path + 'true.npy', trues)
 
-        return mse, mae
+        metrics = {}
+        metrics['mae'] = mae 
+        metrics['mse'] = mse
+        metrics['rmse'] = rmse
+        metrics['mape'] = mape
+        metrics['mspe'] = mspe
+        
+        save_experiment_data(self.args, metrics, preds, trues, self.args.exp_info, self.args.exp_protocol)
+        return
     
